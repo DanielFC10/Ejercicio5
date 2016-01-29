2316c68e436b377a3f23dd2bc4fed560f237ae68
Daniel <Daniel@User-PC>
2016-01-29 18:03:33 -0500

    Gnome

--- Linux/src/linux/Linux.java
+++ Linux/src/linux/Linux.java
@@ -16,6 +16,7 @@ public class Linux {
      */
     public static void main(String[] args) {
         System.out.println("Linux v1.0");
+        System.out.println("Interfaz Grafica Gnome");
     }
     
 }
